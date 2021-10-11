1e43c56d53d210b27732a634505da4e7ed3add05
FilipCysEk <krzysiek.swal@gmail.com>
2021-10-11 12:40:09 +0200

    Create basic allocation examples.

--- allocation_basics/main.cpp
+++ allocation_basics/main.cpp
@@ -3,35 +3,30 @@
 //
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    int l = stoi(argv[2]);
-    cout << l <<endl;
-
-    int *n;
-    n = new int;
+    cout <<argv[1] <<endl;
+    int *n = new int;
     int k;
     cout << n << endl << *n << endl << &n << endl << &k << endl;
-
     delete n;
 
     int *tab = new int[5];
     delete[] tab;
-
     int **tab2 = new int *[5];
 
     for (int i = 0; i < 5; ++i) {
         tab2[i] = new int[5];
     }
 
+    for (int i = 0; i < 5; ++i) {
+        delete[] tab2[i];
+    }
+
+    delete[] tab2;
+
     int tab3[3] = {3, 2, 1};
-
-    tab3[1] = 3 ;
-    tab3[2] = 201;
-
     return 0;
 }
-
